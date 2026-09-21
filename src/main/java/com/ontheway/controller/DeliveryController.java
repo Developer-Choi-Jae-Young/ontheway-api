3704ab4a38d0381b50b97d70fbe3bfbe2c19d0f6
최재영 <cjy951213@naver.com>
2026-09-22 01:04:00 +0900

    fix: 인증/인가 추가로 인한 history, delivery Authenticate 정보 가져오는 코드 추가

--- src/main/java/com/ontheway/controller/DeliveryController.java
+++ src/main/java/com/ontheway/controller/DeliveryController.java
@@ -3,10 +3,12 @@ package com.ontheway.controller;
 import com.ontheway.dto.request.*;
 import com.ontheway.dto.response.*;
 import com.ontheway.global.response.ApiResponse;
+import com.ontheway.global.security.CustomUserDetails;
 import com.ontheway.service.DeliveryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,36 +26,36 @@ public class DeliveryController {
 
     @GetMapping
     @Operation(summary = "이동 경로 게시글 상세 조회")
-    public ApiResponse<DeliveryDetailResponseDto> detail(DeliveryDetailRequestDto deliveryDetailRequestDto) {
-        Long userId = 0L;
+    public ApiResponse<DeliveryDetailResponseDto> detail(@AuthenticationPrincipal CustomUserDetails customUserDetails,  DeliveryDetailRequestDto deliveryDetailRequestDto) {
+        Long userId = customUserDetails.getUserId();
         return ApiResponse.success(deliveryService.detail(userId, deliveryDetailRequestDto));
     }
 
     @PostMapping
     @Operation(summary = "이동 경로 게시글 등록")
-    public ApiResponse<DeliverySaveResponseDto> create(@RequestBody DeliverySaveRequestDto deliverySaveRequestDto) {
-        Long userId = 0L;
+    public ApiResponse<DeliverySaveResponseDto> create(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody DeliverySaveRequestDto deliverySaveRequestDto) {
+        Long userId = customUserDetails.getUserId();
         return ApiResponse.success(deliveryService.create(userId, deliverySaveRequestDto));
     }
 
     @PatchMapping
     @Operation(summary = "이동 경로 게시글 수정")
-    public ApiResponse<DeliveryUpdateResponseDto> update(@RequestBody DeliveryUpdateRequestDto deliveryUpdateRequestDto) {
-        Long userId = 0L;
+    public ApiResponse<DeliveryUpdateResponseDto> update(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody DeliveryUpdateRequestDto deliveryUpdateRequestDto) {
+        Long userId = customUserDetails.getUserId();
         return ApiResponse.success(deliveryService.update(userId, deliveryUpdateRequestDto));
     }
 
     @DeleteMapping("/{id}")
     @Operation(summary = "이동 경로 게시글 삭제")
-    public ApiResponse<DeliveryDeleteResponseDto> delete(@PathVariable Long id) {
-        Long userId = 0L;
+    public ApiResponse<DeliveryDeleteResponseDto> delete(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable Long id) {
+        Long userId = customUserDetails.getUserId();
         return ApiResponse.success(deliveryService.delete(userId, id));
     }
 
     @GetMapping("/me/list")
     @Operation(summary = "내 게시글 이동 경로 목록 조회")
-    public ApiResponse<MyBoardDeliveryListResponseDto> myList(MyBoardDeliveryListRequestDto myBoardDeliveryListRequestDto) {
-        Long userId = 0L;
+    public ApiResponse<MyBoardDeliveryListResponseDto> myList(@AuthenticationPrincipal CustomUserDetails customUserDetails, MyBoardDeliveryListRequestDto myBoardDeliveryListRequestDto) {
+        Long userId = customUserDetails.getUserId();
         return ApiResponse.success(deliveryService.myList(userId, myBoardDeliveryListRequestDto));
     }
 }
--- src/main/java/com/ontheway/controller/HistoryController.java
+++ src/main/java/com/ontheway/controller/HistoryController.java
@@ -3,10 +3,12 @@ package com.ontheway.controller;
 import com.ontheway.dto.request.HistoryListRequestDto;
 import com.ontheway.dto.response.HistoryListResponseDto;
 import com.ontheway.global.response.ApiResponse;
+import com.ontheway.global.security.CustomUserDetails;
 import com.ontheway.service.HistoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,29 +22,29 @@ public class HistoryController {
 
     @GetMapping("/list")
     @Operation(summary = "이용 내역 전체 목록 조회")
-    public ApiResponse<HistoryListResponseDto> list(HistoryListRequestDto historyListRequestDto) {
-        Long userId = 0L;
+    public ApiResponse<HistoryListResponseDto> list(@AuthenticationPrincipal CustomUserDetails customUserDetails, HistoryListRequestDto historyListRequestDto) {
+        Long userId = customUserDetails.getUserId();
         return ApiResponse.success(historyService.list(userId, historyListRequestDto));
     }
 
     @GetMapping("/delivery/list")
     @Operation(summary = "이용 내역 이동 경로 목록 조회")
-    public ApiResponse<HistoryListResponseDto> deliveryList(HistoryListRequestDto historyListRequestDto) {
-        Long userId = 0L;
+    public ApiResponse<HistoryListResponseDto> deliveryList(@AuthenticationPrincipal CustomUserDetails customUserDetails, HistoryListRequestDto historyListRequestDto) {
+        Long userId = customUserDetails.getUserId();
         return ApiResponse.success(historyService.deliveryList(userId, historyListRequestDto));
     }
 
     @GetMapping("/request/list")
     @Operation(summary = "이용 내역 의뢰 요청 목록 조회")
-    public ApiResponse<HistoryListResponseDto> requestList(HistoryListRequestDto historyListRequestDto) {
-        Long userId = 0L;
+    public ApiResponse<HistoryListResponseDto> requestList(@AuthenticationPrincipal CustomUserDetails customUserDetails, HistoryListRequestDto historyListRequestDto) {
+        Long userId = customUserDetails.getUserId();
         return ApiResponse.success(historyService.requestList(userId, historyListRequestDto));
     }
 
     @GetMapping("/cancel/list")
     @Operation(summary = "취소/실패 목록 조회")
-    public ApiResponse<HistoryListResponseDto> cancelList(HistoryListRequestDto historyListRequestDto) {
-        Long userId = 0L;
+    public ApiResponse<HistoryListResponseDto> cancelList(@AuthenticationPrincipal CustomUserDetails customUserDetails, HistoryListRequestDto historyListRequestDto) {
+        Long userId = customUserDetails.getUserId();
         return ApiResponse.success(historyService.cancelList(userId, historyListRequestDto));
     }
 }
